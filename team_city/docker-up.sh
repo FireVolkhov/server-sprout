d78fd46af5534a0f87036dd07c4d57a1a7890dc9
FireVolkhov <firevolkhov@gmail.com>
2016-12-29 20:04:39 +0500

    Фикс

--- team_city/docker-up.sh
+++ team_city/docker-up.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source "$SCRIPT_DIR/../env.sh"
 export DOCKER_PORT="3002"
 
-docker-compose -f "$SCRIPT_DIR/../docker/docker-compose.yml" up -d --project-name "$PROJECT"
+docker-compose -f "$SCRIPT_DIR/../docker/docker-compose.yml" --project-name "$PROJECT" up -d
