203d37930d31ef3cdcbea1c774be3ea40f0b3e84
FireVolkhov <firevolkhov@gmail.com>
2016-12-29 19:38:15 +0500

    Фикс

--- team_city/docker-build.sh
+++ team_city/docker-build.sh
@@ -5,7 +5,7 @@ source "$SCRIPT_DIR/../env.sh"
 
 FULL_VERSION="${VERSION}.0.0.${BUILD_NUMBER:-99}"
 IMAGE="unit6/${PROJECT}"
-ID=$(docker build  -t ${IMAGE}:${FULL_VERSION} -t ${IMAGE}:latest -t $192.168.0.5:5000/{IMAGE}:latest .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
+ID=$(docker build  -t ${IMAGE}:${FULL_VERSION} -t ${IMAGE}:latest -t $192.168.0.5:5000/${IMAGE}:latest .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
 
 #docker tag "$IMAGE" "192.168.0.5:5000/$IMAGE"
 docker push "192.168.0.5:5000/$IMAGE:latest"
